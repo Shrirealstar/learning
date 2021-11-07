f288482d7315d0bf64af5c21ae484041afd06e5b
Shrirealstar <Shrishankarnag@gmail.com>
2021-11-07 16:41:09 +0530

    intial release

---
+++ Project_Euler/Largest_Prime_Factor.py
@@ -0,0 +1,18 @@
+num=int(input("Enter a Number :"))
+def Largert_Prime_Factor(n):
+    prime_factor = 1
+    i = 2
+    
+    while i <= n / i:
+        if n % i == 0:
+            prime_factor = i
+            n /=i
+        else:
+            i += 1
+        
+        if prime_factor < n:
+            prime_factor = n
+        
+    return prime_factor
+    
+print(Largert_Prime_Factor(num))
