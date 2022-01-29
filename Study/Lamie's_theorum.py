271fc70f1c543fd87b9a05061ed30108750972c6
Shrirealstar <Shrishankarnag@gmail.com>
2022-01-29 19:46:49 +0530

    intial release

--- Study/Lamie's_theorum.py
+++ Study/Lamie's_theorum.py
@@ -8,14 +8,39 @@ sin1=int(input("Enter sin1 : "))
 sin2=int(input("Enter sin2 : "))
 sin3=int(input("Enter sin3 : "))
 
+
+#T3 is given and wanted to find out T1
 n=T3*math.sin(math.radians(sin1))/math.sin(math.radians(sin3))
 
-print('T1 is',n)
+print('If T3 given, then the Value of T1 is :',n)
 
-n1=T2*math.sin(math.radians(sin1))/math.sin(math.radians(sin3))
+#T3 is given and wanted to find out T2
+n3=T3*math.sin(math.radians(sin2))/math.sin(math.radians(sin3))
 
-print('T1 is',n1)
+print('If T3 given, then the Value of T2 is :',n3)
 
+#T2 is given and wanted to find out T3
+n4=T2*math.sin(math.radians(sin3))/math.sin(math.radians(sin2))
+
+print('If T2 given, then the Value of T3 is :',n4)
+#T2 is given and wanted to find out T1
+n1=T2*math.sin(math.radians(sin1))/math.sin(math.radians(sin2))
+
+print('If T2 given, then the Value of T1 is :',n1)
+
+#T1 is given and wanted to find out T3
+n5=T1*math.sin(math.radians(sin3))/math.sin(math.radians(sin1))
+
+print('If T1 given, then the Value of T3 is :',n5)
+
+#T1 is given and wanted to find out T2
 n2=T1*math.sin(math.radians(sin2))/math.sin(math.radians(sin1))
 
-print('T1 is',n2)
+print('If T1 given, then the Value of T2 is :',n2)
+
+
+
+
+
+
+
