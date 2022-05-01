2434de489f1d2794513c297b0398fe9b34b9de93
Shrirealstar <Shrishankarnag@gmail.com>
2022-05-01 15:37:06 +0530

    intial release

---
+++ quant_apt/Average_of_4_consiqutive_even_number.py
@@ -0,0 +1,13 @@
+import random
+
+for num in range (1):
+    num1 = random.randint(20,50)
+    a = str(num1)
+
+command = ("The average of four consecutive even number is "+ a + ". Find the largest of these numbers.")
+print(command)
+
+sum = 4*num1
+sum1 = sum-12
+sum2 = sum1/4
+print("largest numbers  = ", sum2+6 )
